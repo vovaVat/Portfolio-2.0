75bc32262fd00e7f51f9048844ee0e3970f32c54
Dmytro Koval <kdmytro793@gmail.com>
2024-11-23 16:26:48 +0200

    fixjsmob

--- src/js/mob_menu.js
+++ src/js/mob_menu.js
@@ -1,21 +1,30 @@
 const menuBtn = document.querySelector(".menu-btm");
 const mobMenu = document.querySelector(".mob-menu");
 const mobMenuClose = document.querySelector(".mob-menu-close");
-const mobMenuItems = document.querySelectorAll(".mob-list-item");
+const mobMenuItems = document.querySelectorAll(".mob-nav");
 const header = document.querySelector(".page-header");
 const main = document.querySelector("main");
 const footer = document.querySelector("footer")
 
-menuBtn.addEventListener("click", () => {
-    mobMenu.classList.add("active");
+menuBtn.addEventListener("click", onMenu);
+
+function onMenu() {
+  mobMenu.classList.add("active");
     header.classList.add("hidden");
     main.classList.add("hidden");
     footer.classList.add("hidden");
-});
+}
 
-mobMenuClose.addEventListener("click", () => {
-    mobMenu.classList.remove("active");
+mobMenuClose.addEventListener("click", closeMenu);
+
+function closeMenu() {
+       mobMenu.classList.remove("active");
     header.classList.remove("hidden");
     main.classList.remove("hidden");
     footer.classList.remove("hidden");
-});
+}
+
+
+
+mobMenuItems.forEach(item => 
+    item.addEventListener("click", closeMenu));
